0bd3a74d97bf57aab643caeb4153a6fcd40b430d
JeremyC57 <jcasadaelliott@mail.csuchico.edu>
2019-09-23 16:12:09 -0700

    Asks for User's First Name and Last Name

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
-//Authors:
+//Authors: Jeremy Casada
 #include<iostream>
 #include<string>
-
 using namespace std;
 
 int main()
 {
   //declare variables
-
+  
+  string firstName;
+  string lastName;
+  
   //get user input
 
+  cout << "What is Your First Name?\n";
+  cin >> firstName;
+
+  cout << "What is your Last Name?\n";
+  cin >> lastName;
+
   //tell fortune
+  
+
 
   return 0;
 }
